2a32544e9ea7d6276097556965e1ee27dd155ffe
illya-onyshchuk <llayonys@gmail.com>
2023-11-14 22:42:47 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+   Divider,
+   List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  ListItemButton,
+  Box,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  CssBaseline,
+  Button,
+  Drawer,
+  Hedden,
+  Hidden
+} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import InboxIcon from '@mui/icons-material/Menu';
+import MailIcon from '@mui/icons-material/Mail';
+import { useState } from 'react';
 
 function App() {
+  const [isDrawerOpen, setDrawerOpen] = useState(false);
+
+  const list = () => (
+    <Box
+      sx={{ width: 250 }}
+      role="presentation" >
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline/>
+      <div>
+        <Box>
+        <AppBar position="static">
+          <Toolbar>
+            <Hidden only={['lg', 'xl']}>
+              <IconButton
+                onClick={() => setDrawerOpen(true)} 
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+              >
+                <MenuIcon/>
+              </IconButton>
+            </Hidden>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              News
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+
+      <Drawer
+        anchor='left'
+        open={isDrawerOpen}
+        onClose={() => setDrawerOpen(false)}
+      >
+        {list()}
+      </Drawer>
+      </Box>
+      </div>
+    </>
   );
 }
 
 export default App;
+ 
